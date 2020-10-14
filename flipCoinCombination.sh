68722a65aec777327b9b581fcd0c00fb19835b9c
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-10-15 01:36:56 +0530

    Multiple flipping Singlet Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 echo "WELCOME TO FLIP COIN COMBINATION"
 
-a=$(( RANDOM%2 ));
-if [ $a == 1 ];
-then
-        echo "Heads";
-else
-        echo "Tails";
-fi
+head=0;
+tail=0;
+read -p "Enter number of flip required " n
+for (( i=0; i<n; i++ ))
+do
+	a=$(( RANDOM%2 ));
+	if [ $a == 1 ];
+	then
+         	head=$(($head+1))
+	else
+        	tail=$(($tail+1))
+	fi
+done
+
+echo "Number of head win" $head;
+
+echo "Number of tail win" $tail;
